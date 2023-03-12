6ca39d004b0c610cd30dbf1a4d415a2f49494cb6
LauritaR <laurita.radisevska@mif.stud.vu.lt>
2023-03-12 23:08:43 +0200

    Update vectorprog.cpp

--- vectorprog.cpp
+++ vectorprog.cpp
@@ -5,18 +5,41 @@ int main()
     vector<studentukas> mas;
     studentukas laikinas;
     string pavadinimas,ats;
-    while(ats!="f"&&ats!="i")
+
+    while(ats!="f"&&ats!="i"&&ats!="g"&&ats!="r")
     {
-        cout<<"duomenu ivedimas ranka(i) ar nuskaitymas is failo(f)?(i/f)";
+        cout<<"-Duomenu ivedimas ranka(i)\n-Nuskaitymas is failo(f)\n-Failo generavimas(g)\n-Failo rusiavimas(r)\n----Iveskite raide----\n";
         cin>>ats; 
     }
     if(ats=="f")
-    {
+    {  
         cout<<"Irasykite failo pavadinima: ";
         cin>>pavadinimas;
-        skaitymas(pavadinimas,"output.txt");  
+     
+        skaitymas(pavadinimas,mas); 
+        rasymas("output.txt",mas);
         
-        cout<<"\nStudentu duomenys sekmingai isvesti i faila 'output.txt'";
+        cout<<"\nStudentu duomenys sekmingai isvesti i faila 'output.txt'";   
+    }
+    else if(ats=="g")
+    {
+        cout<<"Irasykite failo pavadinima: ";
+        cin>>pavadinimas; 
+        auto start = std::chrono::high_resolution_clock::now(); 
+        fileGen(pavadinimas);  
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> difference = end - start;
+        cout <<"visas programos laikas:" << difference.count() << " s\n"; 
+    }
+    else if(ats=="r")
+    {
+        cout<<"Irasykite failo pavadinima: ";
+        cin>>pavadinimas; 
+        auto start = std::chrono::high_resolution_clock::now(); 
+        skirstymas(pavadinimas);  
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> difference = end - start;
+        cout <<"visas programos laikas:" << difference.count() << " s\n"; 
     }
     else if(ats=="i")
     {
@@ -24,15 +47,14 @@ int main()
         pildyk(laikinas);
         mas.push_back(laikinas);
         laikinas.pazymiukai.clear();
-        
+
         while((ats!="t")&&(ats!="n"))
         {
         cout<<"Ar norite ivesti informacija apie kitus studentus?(t/n)?";
         cout<<"t-prideti dar studenta, n-baigti ivedima"<<endl;
         cin>>ats;
         }
-    }while(ats=="t");
-
+        }while(ats=="t");
     cout<<setw(30)<<left<<"Vardas"<<setw(30)<<left<<"Pavarde"<<setw(30)<<left<<"Galutinis(vid)"<<setw(30)<<left<<"Galutinis(med)"<<endl;
     cout<<setw(20)<<"-----------------------------------------------------------------------------------------------------------"<<endl; 
     for(auto &i: mas)
@@ -44,7 +66,7 @@ int main()
       i.pazymiukai.clear();
    }
    mas.clear();   
-}
+    }
 
 return 0;
 }
