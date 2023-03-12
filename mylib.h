9a5b271650da7c0fafd6478b6931685d8bc9bed1
LauritaR <laurita.radisevska@mif.stud.vu.lt>
2023-03-12 23:09:26 +0200

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -5,10 +6,14 @@
 #include <vector>
 #include <limits>
 #include <time.h>
+#include <fstream>
+#include <chrono>
+#include <omp.h>
+using std::partition;
 using std::cout;
 using std::cin;
 using std::endl;
-
+using std::back_inserter;
 using std::left;
 using std::right;
 using std::setw;
@@ -20,3 +25,10 @@ using std::sort;
 using std::vector;
 using std::getline;
 using std::fixed;
+using std::stringstream;
+using std::runtime_error;
+using std::ifstream;
+using std::ofstream;
+using std::cerr;
+using std::istringstream;
+using std::move;
