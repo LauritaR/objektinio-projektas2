4b6e3a6a717b0d1f3b993dbc989f45ac23472a08
LauritaR <laurita.radisevska@mif.stud.vu.lt>
2023-03-12 23:09:12 +0200

    Update fun.h

--- fun.h
+++ fun.h
@@ -13,5 +13,10 @@ float galutinisMed(studentukas &temp);
 bool sortPavarde(studentukas& a, studentukas& b);
 bool sortVardas(studentukas&a, studentukas&b);
 void spausdinimas(studentukas &temp);
-void skaitymas(string read_studentukas, string write_studentukas);
+void skaitymas(string read_studentukas, vector<studentukas>& stud); 
+void fileGen(string& filename);
+void rasymas(string write_studentukas, vector<studentukas>& stud);
+string randName();
+string randSur();
 
+void skirstymas(string filename);
