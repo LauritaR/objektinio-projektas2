b7d2955ab5143faee74c5242a4e4aa6d7065241c
LauritaR <laurita.radisevska@mif.stud.vu.lt>
2023-04-17 18:55:17 +0300

    1stretegija/list

---
+++ 1strategija/list/fun.h
@@ -0,0 +1,13 @@
+#include "mylib.h"
+
+void pildyk(studentukas &temp);
+float galutinisVID(studentukas &temp);
+float galutinisMed(studentukas &temp);
+void spausdinimas(studentukas &temp);
+void skaitymas(string read_studentukas, list<studentukas>& stud); 
+void fileGen(string& filename);
+void rasymas(string write_studentukas, list<studentukas>& stud);
+string randName();
+string randSur();
+void skirstymas(string filename);
+
