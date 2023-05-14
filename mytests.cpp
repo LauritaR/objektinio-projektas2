3e257b77d52d4ce0e83709ad282d46a2260f94d7
Laurita <116920291+LauritaR@users.noreply.github.com>
2023-05-14 13:03:48 +0300

    tests

--- mytests.cpp
+++ mytests.cpp
@@ -4,42 +4,42 @@
 
 TEST_CASE("File generation works correctly", "[fileGen]") {
 
-    string filename = "test.txt";
+    string pavadinimas = "test.txt";
   
-    fileGen(filename);
+    fileGen(pavadinimas);
 
-    ifstream fin(filename);
+    ifstream fin(pavadinimas);
     REQUIRE(fin.is_open());
     fin.close();
 
-    ifstream fin2(filename);
+    ifstream fin2(pavadinimas);
     string line;
     getline(fin2, line);
     stringstream ss(line);
-    string word;
-    ss >> word;
-    REQUIRE(word == "Vardas");
-    ss >> word;
-    REQUIRE(word == "Pavardė");
+    string w;
+    ss >> w;
+    REQUIRE(w == "Vardas");
+    ss >> w;
+    REQUIRE(w == "Pavardė");
     for (int i = 1; i <= 5; i++) {
-        ss >> word;
-        REQUIRE(word == ("ND" + std::to_string(i)));
+        ss >> w;
+        REQUIRE(w == ("ND" + std::to_string(i)));
     }
-    ss >> word;
-    REQUIRE(word == "Egz.");
+    ss >> w;
+    REQUIRE(w == "Egz.");
 
     for (int i = 1; i <= 10; i++) {
         getline(fin2, line);
         stringstream ss2(line);
-        string name, surname;
-        ss2 >> name >> surname;
-        REQUIRE(name.length() <= 15);
-        REQUIRE(surname.length() <= 15);
+        string vardas, pavarde;
+        ss2 >> vardas >> pavarde;
+        REQUIRE(vardas.length() <= 15);
+        REQUIRE(pavarde.length() <= 15);
         for (int j = 1; j <= 5; j++) {
-            int grade;
-            ss2 >> grade;
-            REQUIRE(grade >= 1);
-            REQUIRE(grade <= 10);
+            int paz;
+            ss2 >> paz;
+            REQUIRE(paz >= 1);
+            REQUIRE(paz <= 10);
         }
         int egz;
         ss2 >> egz;
@@ -48,22 +48,20 @@ TEST_CASE("File generation works correctly", "[fileGen]") {
     }
     fin2.close();
 
-    remove(filename.c_str());
+    remove(pavadinimas.c_str());
 }
-
-
 
 
 TEST_CASE("Testing rasymas function") {
     std::vector<studentukas> stud;
-    stud.emplace_back("Jonas", "Jonaitis", std::vector<int>{10, 9, 8, 7, 10},5); // add sample student
-    std::ostringstream oss; // create output string stream
-    rasymas("test.txt", stud); // call rasymas function with test data
-    std::ifstream in("test.txt"); // open test file
-    oss << in.rdbuf(); // read contents of file into output string stream
-    std::string output = oss.str(); // get string from output string stream
+    stud.emplace_back("Jonas", "Jonaitis", std::vector<int>{10, 9, 8, 7, 10},5); 
+    std::ostringstream oss;
+    rasymas("test.txt", stud);
+    std::ifstream in("test.txt"); 
+    oss << in.rdbuf(); 
+    std::string output = oss.str(); 
     std::string expected = "Vardas              Pavardė            Galutinis(vid)      \n"
                            "---------------------------------------------------------------------------\n"
-                           "Jonas               Jonaitis            6.52\n"; // define expected output
-    REQUIRE(output == expected); // compare actual and expected output
+                           "Jonas               Jonaitis            6.52\n"; 
+    REQUIRE(output == expected); 
 }
