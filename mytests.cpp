d4f8bb6c66e37a801bcd1c8eec5c109c85b84ae1
Laurita <116920291+LauritaR@users.noreply.github.com>
2023-05-29 15:09:13 +0300

    updated mytests.cpp

--- mytests.cpp
+++ mytests.cpp
@@ -2,9 +2,9 @@
 #include "catch.hpp"
 #include "fun.h"
 
-TEST_CASE("File generation works correctly", "[fileGen]") {
+TEST_CASE("Failų generavimo testavimas", "[fileGen]") {
 
-    string pavadinimas = "test.txt";
+    string pavadinimas = "test_output.txt";
   
     fileGen(pavadinimas);
 
@@ -47,12 +47,10 @@ TEST_CASE("File generation works correctly", "[fileGen]") {
         REQUIRE(egz <= 10);
     }
     fin2.close();
-
-    remove(pavadinimas.c_str());
 }
 
 
-TEST_CASE("Testing rasymas function") {
+TEST_CASE("Rašymo į failą testavimas") {
     std::vector<studentukas> stud;
     stud.emplace_back("Richard", "Feynman", std::vector<int>{10, 9, 8, 7, 10},5); 
     std::ostringstream oss;
@@ -64,4 +62,114 @@ TEST_CASE("Testing rasymas function") {
                            "---------------------------------------------------------------------------\n"
                            "Richard             Feynman             6.52\n"; 
     REQUIRE(output == expected); 
+}
+
+TEST_CASE("Rule of Five - studentukas class") {
+    // Test case 1: Copy constructor
+    SECTION("Copy constructor") {
+        studentukas obj1("Studentas", "Studentaitis", {5, 4, 10}, 10);
+        studentukas obj2(obj1);
+
+        std::cout << "Copy constructor:\n";
+        std::cout << "obj1: " << obj1 << "\n";
+        std::cout << "obj2: " << obj2 << "\n";
+
+        REQUIRE(obj1.getVardas() == obj2.getVardas());
+        REQUIRE(obj1.getPavarde() == obj2.getPavarde());
+        REQUIRE(obj1.getPazymiai() == obj2.getPazymiai());
+        REQUIRE(obj1.getEgzas() == obj2.getEgzas());
+    }
+
+    // Test case 2: Copy assignment operator
+    SECTION("Copy assignment operator") {
+        studentukas obj1("Studentas", "Studentaitis", {5, 4, 10}, 10);
+        studentukas obj2;
+        obj2 = obj1;
+
+        std::cout << "Copy assignment operator:\n";
+        std::cout << "obj1: " << obj1 << "\n";
+        std::cout << "obj2: " << obj2 << "\n";
+
+        REQUIRE(obj1.getVardas() == obj2.getVardas());
+        REQUIRE(obj1.getPavarde() == obj2.getPavarde());
+        REQUIRE(obj1.getPazymiai() == obj2.getPazymiai());
+        REQUIRE(obj1.getEgzas() == obj2.getEgzas());
+    }
+
+    // Test case 3: Move constructor
+    SECTION("Move constructor") {
+        studentukas obj1("Studentas", "Studentaitis", {5, 4, 10}, 10);
+        studentukas obj2(std::move(obj1));
+
+        std::cout << "Move constructor:\n";
+        std::cout << "obj1: " << obj1 << "\n";
+        std::cout << "obj2: " << obj2 << "\n";
+
+        REQUIRE(obj1.getVardas().empty());
+        REQUIRE(obj1.getPavarde().empty());
+        REQUIRE(obj1.getPazymiai().empty());
+        REQUIRE(obj1.getEgzas() == 0);
+
+        REQUIRE(obj2.getVardas() == "Studentas");
+        REQUIRE(obj2.getPavarde() == "Studentaitis");
+        REQUIRE(obj2.getPazymiai() == std::vector<int>({5, 4, 10}));
+        REQUIRE(obj2.getEgzas() == 10);
+    }
+
+    // Test case 4: Move assignment operator
+    SECTION("Move assignment operator") {
+        studentukas obj1("Studentas", "Studentaitis", {5, 4, 10}, 10);
+        studentukas obj2;
+        obj2 = std::move(obj1);
+
+        std::cout << "Move assignment operator:\n";
+        std::cout << "obj1: " << obj1 << "\n";
+        std::cout << "obj2: " << obj2 << "\n";
+
+        REQUIRE(obj1.getVardas().empty());
+        REQUIRE(obj1.getPavarde().empty());
+        REQUIRE(obj1.getPazymiai().empty());
+        REQUIRE(obj1.getEgzas() == 0);
+
+        REQUIRE(obj2.getVardas() == "Studentas");
+        REQUIRE(obj2.getPavarde() == "Studentaitis");
+        REQUIRE(obj2.getPazymiai() == std::vector<int>({5, 4, 10}));
+        REQUIRE(obj2.getEgzas() == 10);
+    }
+}
+
+class TestClass : public zmogus {
+public:
+    using zmogus::zmogus; 
+
+    void setVardas(string v) override {
+        vardas = v;
+    }
+
+    void setPavarde(string p) override {
+        pavarde = p;
+    }
+};
+
+TEST_CASE("zmogus Abstract Class") {
+    SECTION("Constructor and Getters") {
+        TestClass obj("Studentas", "Studentaitis");
+        cout<<"Vardas: "<<obj.getVardas()<<endl;
+        cout<<"Pavardė: "<<obj.getPavarde()<<endl;
+
+        REQUIRE(obj.getVardas() == "Studentas");
+        REQUIRE(obj.getPavarde() == "Studentaitis");
+    }
+
+    SECTION("Setters") {
+        TestClass obj;
+
+        obj.setVardas("Studentas");
+        obj.setPavarde("Studentaitis");
+        cout<<"Vardas: "<<obj.getVardas()<<endl;
+        cout<<"Pavardė: "<<obj.getPavarde()<<endl; 
+
+        REQUIRE(obj.getVardas() == "Studentas");
+        REQUIRE(obj.getPavarde() == "Studentaitis");
+    }
 }
